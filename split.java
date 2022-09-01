43f6b5c08d05fd306bce49f1633484cc9b0a231a
SreelakshmiNair200329 <108935120+SreelakshmiNair200329@users.noreply.github.com>
2022-09-01 17:46:57 +0530

    Update split.java

--- split.java
+++ split.java
@@ -1,6 +1,6 @@
 public class split{  
 public static void main(String args[]){  
-String s1="HI! I'm Sreelaxmi Nair and my hobbies are cricket,taveling,listening music  and many more thing,Thank you.";  
+String s1="HI! I'm Sreelashmi Nair and my hobbies are cricket,taveling,listening music  and many more thing,Thank you.";  
 String[] words=s1.split("\\s");//splits the string based on whitespace  
 //using java foreach loop to print elements of string array  
 for(String w:words){  
